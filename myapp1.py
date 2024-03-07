b7bd93eb735d919cc2a3a230b13cf9d5f82f2888
benderrusso1 <162640260+benderrusso1@users.noreply.github.com>
2024-03-07 16:54:48 -0600

    Add files via upload

---
+++ myapp1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(layout="wide")
+
+# Embed codes for different Tableau dashboards
+dashboard_embed_codes = {
+    "Illegal Pat Down": '''
+        <iframe src="https://prod-useast-b.online.tableau.com/t/benderrusso2e46bd1bb7/views/jp_streamlit/Dashboard2?:showVizHome=no&:embed=true" width="100%" height="840"></iframe>
+    ''',
+    "RAS": '''
+        <iframe src="https://prod-useast-b.online.tableau.com/t/benderrusso2e46bd1bb7/views/jp_streamlit/Dashboard1?:showVizHome=no&:embed=true" width="100%" height="840"></iframe>
+    ''',
+    "Body and Car Camera": '''
+        <iframe src="https://prod-useast-b.online.tableau.com/t/benderrusso2e46bd1bb7/views/jp_streamlit/Dashboard3?:showVizHome=no&:embed=true" width="100%" height="800"></iframe>
+    ''',
+   
+}
+
+def main():
+    st.title('Chicago Police ISR')
+
+    # Creating tabs for different dashboards
+    tab_labels = list(dashboard_embed_codes.keys())
+    tabs = st.tabs(tab_labels)
+    
+    for i, tab_label in enumerate(tab_labels):
+        with tabs[i]:
+            st.write(f"This is the {tab_label}")
+            components.html(dashboard_embed_codes[tab_label], height=800)
+
+if __name__ == "__main__":
+    main()
